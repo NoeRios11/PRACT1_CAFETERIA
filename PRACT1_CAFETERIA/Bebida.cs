573417c07a92e1bf71f7b8fcea7f794bf9601894
Noe Rios de la Cruz <noeriosdelacruz715@gmail.com>
2026-05-19 15:26:39 -0600

    Agregar archivos de proyecto.

---
+++ PRACT1_CAFETERIA/Bebida.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACT1_CAFETERIA
+{
+    internal class Bebida
+    {
+        private string nombre;
+        private string tamano;
+        private float precio;
+
+        public Bebida(string nombre, string tamano, float precio)
+        {
+            this.nombre = nombre;
+            this.tamano = tamano;
+            this.precio = precio;
+        }
+
+        public void Preparar()
+        {
+            Console.WriteLine($"PREPARANDO UN {nombre} TAMAÑO {tamano}...");
+        }
+
+        public void AplicarDescuento(float porcentaje)
+        {
+            float descuento = precio * (porcentaje / 100);
+            precio = precio - descuento;
+        }
+
+        public void MostrarDescripcion()
+        {
+            Console.WriteLine($"ESTO ES {nombre} DEL TAMAÑO {tamano} CON UN COSTO DE ${precio}");
+        }
+    }
+}
---
+++ PRACT1_CAFETERIA/Program.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACT1_CAFETERIA
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nombre;
+            string tamano;
+            float precio;
+
+            // BEBIDA 1
+            Console.WriteLine("PRODUCTOS DISPONIBLES");
+            Console.WriteLine("CAFE"+"\n" + "CHOCOMILK"+"\n"+"CAFE NEGRO");    //MUESTRA LOS PRODUCTOS
+            Console.WriteLine("INGRESE EL NOMBRE DE LA BEBIDA:");
+            nombre = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("TAMAÑOS:");
+            Console.WriteLine("CHICO" + "\n" + "MEDIANO" + "\n" + "GRANDE"); //MUESTRA LOS TAMAÑOS
+            Console.WriteLine("INGRESE EL TAMAÑO DEL "+nombre);
+            tamano = Console.ReadLine();
+            Console.WriteLine("INGRESE EL COSTO");
+            precio = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+            
+            Bebida bebida_1 = new Bebida(nombre, tamano, precio);
+
+
+            //BEBIDA 2
+            Console.WriteLine("PRODUCTOS DISPONIBLES");
+            Console.WriteLine("CAFE" + "\n" + "CHOCOMILK" + "\n" + "CAFE NEGRO");//MUESTRA LOS PRODUCTOS
+            Console.WriteLine("INGRESE EL NOMBRE DE LA BEBIDA:");
+            nombre = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("TAMAÑOS:");
+            Console.WriteLine("CHICO" + "\n" + "MEDIANO" + "\n" + "GRANDE");  //MUESTRA LOS TAMAÑOS
+            Console.WriteLine("INGRESE EL TAMAÑO DEL "+nombre);
+            tamano = Console.ReadLine();
+            Console.WriteLine("INGRESE EL COSTO:");
+            precio = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Bebida bebida_2 = new Bebida(nombre, tamano, precio);
+
+
+            //BEBIDA 3
+            Console.WriteLine("PRODUCTOS DISPONIBLES");
+            Console.WriteLine("CAFE" + "\n" + "CHOCOMILK" + "\n" + "CAFE NEGRO");  //MUESTRA LOS PRODUCTOS
+            Console.WriteLine("INGRESE EL NOMBRE DE LA BEBIDA:");
+            nombre = Console.ReadLine();
+            Console.WriteLine();
+            Console.WriteLine("TAMAÑOS:");
+            Console.WriteLine("CHICO" + "\n" + "MEDIANO" + "\n" + "GRANDE"); //MUESTRA LOS TAMAÑOS
+            Console.WriteLine("INGRESE EL TAMAÑO DEL "+nombre);
+            tamano = Console.ReadLine();
+            Console.WriteLine("INGRESE EL COSTO");
+            precio = float.Parse(Console.ReadLine());
+            Console.WriteLine();
+
+            Bebida bebida_3 = new Bebida(nombre, tamano, precio);
+
+
+
+
+            bebida_1.AplicarDescuento(10);
+            bebida_2.AplicarDescuento(25);
+            bebida_3.AplicarDescuento(35);
+
+            Console.WriteLine("\n PREPARACION DE LAS BEBIDAS");
+            bebida_1.Preparar();
+            bebida_2.Preparar();
+            bebida_3.Preparar();
+
+            Console.WriteLine("\n DESCRIPCION DE LAS BEBIDAS");
+            bebida_1.MostrarDescripcion();
+            bebida_2.MostrarDescripcion();
+            bebida_3.MostrarDescripcion();
+
+            Console.ReadKey();
+        }
+    }
+}
